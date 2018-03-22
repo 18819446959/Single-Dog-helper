fb042f12ae631426ed8fe8cf067c9d61674b05d8
JY.Mr <30524229+JYMr@users.noreply.github.com>
2018-03-22 09:43:12 +0800

    修改

--- bf_jump.js
+++ bf_jump.js
@@ -1,3 +1,10 @@
-setTimeout(()=>{
-	window.location.href="/wap/index/index.html";
-},500)
+
+if($('.take .take-b').length <= 0 || $('.take .btn-box .org').length <= 0){
+	setTimeout(()=>{
+		window.location.href="/wap/index/index.html";
+	},500)
+}else{
+	localStorage.isSend = 1;
+	sendMail();
+	abTitle();
+}
