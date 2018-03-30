7bdab2fcbb05fedc7b035fc349d0a55015e2b6d5
JY.Mr <30524229+JYMr@users.noreply.github.com>
2018-03-30 16:47:57 +0800

    修改

---
+++ test.js
@@ -0,0 +1,3 @@
+$('.hy_state b').html('18819493902');
+$('.zjgl-right_2').eq(0).find('tr').eq(1).find('td').eq(0).html('不小林的小林');
+$('.zjgl-right_2').eq(1).find('tr').eq(1).find('td').eq(0).html('jd188194kwp');
