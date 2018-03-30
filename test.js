2490f3a1acfd1f8f35dc71e968e7cce79e43e729
JY.Mr <30524229+JYMr@users.noreply.github.com>
2018-03-30 17:08:48 +0800

    Revert "修改"
    This reverts commit 7bdab2fcbb05fedc7b035fc349d0a55015e2b6d5.

--- test.js
+++
@@ -1,3 +0,0 @@
-$('.hy_state b').html('18819493902');
-$('.zjgl-right_2').eq(0).find('tr').eq(1).find('td').eq(0).html('不小林的小林');
-$('.zjgl-right_2').eq(1).find('tr').eq(1).find('td').eq(0).html('jd188194kwp');
