5e0082e367331f50e0b2cc59a34ff0b29b448cf7
JYMr <abbyhg@live.com>
2018-03-31 17:40:33 +0800

    修改

--- wk.js
+++ wk.js
@@ -112,8 +112,14 @@
 
 				var timeConfirm = setInterval(function(){
 					//等待回调弹窗
-					if($('#ow_confirm002').length > 0){
-						$('#ow_confirm002_fun')[0].click();
+					let $confirmDialog = $('#ow_confirm002');
+					if($confirmDialog.length > 0){
+						if($confirmDialog.find('.sjzc_5_t:first div:first').text() == '当前排队人数大于100,你是否确认接单？'){
+							$confirmDialog.find('#ow_confirm002_fun2')[0].click();
+							clearInterval(timeConfirm);
+							startMission();
+						}
+						//$confirmDialog[0].click();
 					}
 				},500);
 			},1000)
